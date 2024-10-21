632e0c9068eed1ce35ff66060d6986814aab3840
Benjamin Hamon <development@benjaminhamon.com>
2024-10-21 20:15:02 +0200

    Update metadata

--- Sources/website/benjaminhamon_developer_website/__init__.py
+++ Sources/website/benjaminhamon_developer_website/__init__.py
@@ -1,14 +1,18 @@
+__product__ = None
 __copyright__ = None
 __version__ = None
 __date__ = None
 
 
 try:
-    import benjaminhamon_developer_website.__metadata__
+    import benjaminhamon_developer_website.__metadata__ # type: ignore
 
+    # pylint: disable = no-member
+    __product__ = benjaminhamon_developer_website.__metadata__.__product__
     __copyright__ = benjaminhamon_developer_website.__metadata__.__copyright__
     __version__ = benjaminhamon_developer_website.__metadata__.__version__
     __date__ = benjaminhamon_developer_website.__metadata__.__date__
+    # pylint: enable = no-member
 
 except ImportError:
     pass
