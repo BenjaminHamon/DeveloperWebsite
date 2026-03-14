d5333e38c2568cc17da8f3174265a3229096eed4
Benjamin Hamon <development@benjaminhamon.com>
2026-03-14 18:49:12 +0100

    Add configuration option for open to work

--- Sources/website/benjaminhamon_developer_website/run.py
+++ Sources/website/benjaminhamon_developer_website/run.py
@@ -20,7 +20,7 @@ def main():
 
     configure_logging(arguments)
 
-    application = application_factory.create_application("metrics")
+    application = application_factory.create_application(open_to_work = False, metrics_token = "metrics")
     website_url = "http://%s:%s/" % (arguments.address, arguments.port)
     os.environ["DEBUG_METRICS"] = "1" # For Prometheus exporter
 
